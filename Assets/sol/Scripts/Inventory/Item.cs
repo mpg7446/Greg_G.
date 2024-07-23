4f16148d9eaae83075b46205c1498661c4d83be0
mpg7446 <mpg7446@autuni.ac.nz>
2024-07-23 22:43:20 +1200

    added test gui for delayed item pickup

--- Assets/sol/Scripts/Inventory/Item.cs
+++ Assets/sol/Scripts/Inventory/Item.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
     [SerializeField] private int score = 1;
+
     public float countdown;
     private float countdownLimit;
+
     public bool touching;
     public GameObject player; // this should be used to check if anyone else is touching the item, only the first player to touch the item should pick it up
+
+    public GameObject sliderObj;
+    public Slider slider;
 
     public void Start()
     {
         countdownLimit = countdown;
+
+        if (sliderObj == null)
+        {
+            sliderObj = transform.Find("Slider").gameObject;
+        }
+
+        slider = sliderObj.GetComponent<Slider>();
+        slider.value = 0;
+        slider.maxValue = countdownLimit;
+
+        sliderObj.SetActive(false);
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -22,7 +40,6 @@ public class Item : MonoBehaviour
             // set touching score to positive to begin countdown
             touching = true;
             player = collision.gameObject;
-            Debug.Log("player entered collider");
         }
     }
 
@@ -32,7 +49,6 @@ public class Item : MonoBehaviour
         {
             // set touching score to positive to begin countdown
             touching = false;
-            Debug.Log("player left collider");
         }
     }
 
@@ -41,15 +57,30 @@ public class Item : MonoBehaviour
         if (touching)
         {
             countdown -= 2;
+
             if (countdown < 0)
             {
                 PickupItem();
             }
+
+            UpdateSlider();
         } 
         else if (countdown < countdownLimit)
         {
             countdown += 1.5f;
+
+            UpdateSlider();
+        } 
+        else if (countdown >= countdownLimit)
+        {
+            sliderObj.SetActive(false);
         }
+    }
+
+    private void UpdateSlider()
+    {
+        sliderObj.SetActive(true);
+        slider.value = countdownLimit - countdown;
     }
     
     private void PickupItem()
