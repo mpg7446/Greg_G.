b384900315bfd2e79e664173dc1860eb5f14215c
mpg7446 <mpg7446@autuni.ac.nz>
2024-08-02 13:31:19 +1200

    more stack changes

--- Assets/sol/Scripts/Movement/PlayerMovement.cs
+++ Assets/sol/Scripts/Movement/PlayerMovement.cs
@@ -12,7 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public int id;
 
     private Rigidbody2D rb;
-    private InputManager mv;
+    private InputManager input;
 
     // movement settings
     public float speed = 40;
@@ -20,10 +20,18 @@ public class PlayerMovement : MonoBehaviour
     public float crouchMultiplier = 0.2f;
     private float moving = 0;
 
+    // action settings
+    private bool holdingAction = false;
+
     // player stacking
     private bool inStack;
     public GameObject stackParent;
     public GameObject stackChild;
+
+    private BoxCollider2D boxCollider;
+    private CircleCollider2D circleCollider;
+    private int colliderDelay = 0;
+    [SerializeField] private int colliderMaxDelay = 25;
 
     // player weight
     [SerializeField] private float weight = 100; 
@@ -41,8 +49,12 @@ public class PlayerMovement : MonoBehaviour
 
     public void Start()
     {
-        mv = GetComponent<InputManager>();
+        input = GetComponent<InputManager>();
         rb = GetComponent<Rigidbody2D>();
+
+        boxCollider = GetComponent<BoxCollider2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+        circleCollider.enabled = false;
 
         id = idCounter;
         idCounter++;
@@ -55,69 +67,27 @@ public class PlayerMovement : MonoBehaviour
     // movement - on physics update
     public void FixedUpdate()
     {
-        // movement inputs
-        if (mv.movement[id].x != 0)
+        if (!inStack)
         {
-            // acceleration
-            moving += speed * mv.movement[id].x;
-            if (moving > maxSpeed)
-            {
-                moving = maxSpeed;
-            } 
-            else if (moving < -maxSpeed)
-            {
-                moving = -maxSpeed;
-            }
-
-            // weight burst
-            if (burstTimer > 0)
-            {
-                burstTimer--;
-                bursting = true;
-            }
-            else
-            {
-                bursting = false;
-            }
-        } 
-        else
+            StandardMovement();
+        } else
         {
-            // deceleration
-            if (moving < -maxSpeed/10)
-            {
-                moving += maxSpeed / 10;
-            }
-            else if (moving > maxSpeed/10)
-            {
-                moving -= maxSpeed / 10;
-            } else
-            {
-                moving = 0;
-            }
-
-            // cancel weight burst
-            bursting = false;
-            burstTimer = burstMax;
-        }
-        
-        // update speed and burst mulitpliers
-        float finalSpeed = moving * GetWeightMultiplier(bursting, burstMultiplier);
-
-        // crouch
-        finalSpeed *= Crouch();
-
-        // jump
-        if (!jumped && mv.movement[id].y > 0 && rb.velocity.y == 0)
-        {
-            Jump();
-        } 
-        else
-        {
-            jumped = false;
+            StackMovement();
         }
 
-        Vector2 trans = new Vector3(finalSpeed * Time.fixedDeltaTime, rb.velocity.y);
-        rb.velocity = trans;
+        if (input.action[id] && !holdingAction)
+        {
+            Action();
+            holdingAction = true;
+        } else if (!input.action[id] && holdingAction)
+        {
+            holdingAction = false;
+        }
+
+        if (colliderDelay > 0)
+        {
+            colliderDelay--;
+        }
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
@@ -128,15 +98,18 @@ public class PlayerMovement : MonoBehaviour
 
             GameObject otherPlayer = collision.gameObject;
 
-            if (stackParent == null && transform.position.y > otherPlayer.transform.position.y + (GetComponent<BoxCollider2D>().size.y / 1.1)) // collision is below player and players height
+            if (!inStack && colliderDelay <= 0)
             {
-                Debug.Log("Player " + id + " entered stack from above");
-                EnterStackAbove(otherPlayer);
-            } 
-            else if (stackChild == null && transform.position.y < otherPlayer.transform.position.y - (GetComponent<BoxCollider2D>().size.y / 1.1)) // collision is above player and players height
-            {
-                Debug.Log("Player " + id + " entered stack from below");
-                EnterStackBelow(otherPlayer);
+                if (stackParent == null && transform.position.y > otherPlayer.transform.position.y + (GetComponent<BoxCollider2D>().size.y / 1.1)) // collision is below player and players height
+                {
+                    Debug.Log("Player " + id + " entered stack from above");
+                    EnterStackAbove(otherPlayer);
+                }
+                else if (stackChild == null && transform.position.y < otherPlayer.transform.position.y - (GetComponent<BoxCollider2D>().size.y / 1.1)) // collision is above player and players height
+                {
+                    Debug.Log("Player " + id + " entered stack from below");
+                    EnterStackBelow(otherPlayer);
+                }
             }
         }
     }
@@ -146,7 +119,7 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Player")) // check if not colliding with player
         {
             canBurst = false;
-
+            /*
             GameObject otherPlayer = collision.gameObject;
 
             if (otherPlayer == stackChild)
@@ -165,13 +138,83 @@ public class PlayerMovement : MonoBehaviour
                     inStack = false;
                 }
             }
+            */
         }
     }
 
     // Movement & Action Inputs
     private void StandardMovement() // for movement seperate from player stack
     {
+        SwitchCollider(false);
+        // movement inputs
+        if (input.movement[id].x != 0)
+        {
+            // acceleration
+            moving += speed * input.movement[id].x;
+            if (moving > maxSpeed)
+            {
+                moving = maxSpeed;
+            }
+            else if (moving < -maxSpeed)
+            {
+                moving = -maxSpeed;
+            }
 
+            // weight burst
+            if (burstTimer > 0)
+            {
+                burstTimer--;
+                bursting = true;
+            }
+            else
+            {
+                bursting = false;
+            }
+        }
+        else
+        {
+            // deceleration
+            if (moving < -maxSpeed / 10)
+            {
+                moving += maxSpeed / 10;
+            }
+            else if (moving > maxSpeed / 10)
+            {
+                moving -= maxSpeed / 10;
+            }
+            else
+            {
+                moving = 0;
+            }
+
+            // cancel weight burst
+            bursting = false;
+            burstTimer = burstMax;
+        }
+
+        // update speed and burst mulitpliers
+        float finalSpeed = moving * GetWeightMultiplier(bursting, burstMultiplier);
+
+        // crouch
+        finalSpeed *= Crouch();
+
+        // jump
+        if (!jumped && input.movement[id].y > 0 && rb.velocity.y == 0)
+        {
+            Jump();
+        }
+        else
+        {
+            jumped = false;
+        }
+
+        Vector2 trans = new Vector3(finalSpeed * Time.fixedDeltaTime, rb.velocity.y);
+        rb.velocity = trans;
+    }
+    private void StackMovement() // for movement in a player stack
+    {
+        SwitchCollider(true);
+        rb.velocity = Vector3.up;
     }
     private void Jump()
     {
@@ -180,7 +223,7 @@ public class PlayerMovement : MonoBehaviour
     }
     private float Crouch(bool enabled = true) // call this action squish in game i think
     {
-        if (mv.movement[id].y < 0)
+        if (input.movement[id].y < 0)
         {
             if (transform.localScale == new Vector3(1, 1, 1))
             {
@@ -227,6 +270,8 @@ public class PlayerMovement : MonoBehaviour
     {
         stackParent = parent;
         inStack = true;
+
+        transform.position = parent.transform.position + new Vector3(0, parent.GetComponent<BoxCollider2D>().size.y);
     }
     private void EnterStackBelow(GameObject child)
     {
@@ -235,12 +280,69 @@ public class PlayerMovement : MonoBehaviour
     }
     private void LeaveStack()
     {
-        stackChild = null;
-        stackParent = null;
-        inStack = false;
+        if (stackParent != null && stackChild != null) // if player is a middle player in stack
+        {
+            stackParent.GetComponent<PlayerMovement>().stackChild = stackChild;
+            stackChild.GetComponent<PlayerMovement>().stackParent = stackParent;
+        } 
+        else // if player is an end player in stack
+        {
+            // CHANGE THIS TO A FUNCTION TAHT GETS CALLED SO THAT inStack IS CORRECT
+            if (stackParent != null && stackChild == null)
+            {
+                stackParent.GetComponent<PlayerMovement>().LeaveStack(gameObject);
+            }
+            if (stackChild != null && stackParent == null)
+            {
+                stackChild.GetComponent<PlayerMovement>().LeaveStack(gameObject);
+            }
+        }
 
-        // TODO - change this so it shoots u in the players last facing direction
-        rb.AddForce(new Vector3(maxSpeed * Time.fixedDeltaTime, rb.velocity.y));
+        stackParent = null;
+        stackChild = null;
+        inStack = false;
+        colliderDelay = colliderMaxDelay;
+
+        // TODO - change this so it shoots u in the players last facing direction and disables all stop onColliderEnter from calling EnterStack* for a certain amount of time
+        // OR last movement direction
+        transform.position += new Vector3(0, 0.5f, 0);
+        rb.velocity = new Vector3(maxSpeed * 5 * Time.fixedDeltaTime, 0);
+    }
+    private void LeaveStack(GameObject player)
+    {
+        if (stackChild == player)
+        {
+            stackChild = null;
+        } 
+        else if (stackParent == player)
+        {
+            stackParent = null;
+        }
+
+        if (stackChild == null && stackParent == null)
+        {
+            inStack = false;
+            colliderDelay = colliderMaxDelay;
+        }
+    }
+
+    // Colliders
+    private void SwitchCollider()
+    {
+        boxCollider.enabled = !boxCollider.enabled;
+        circleCollider.enabled = !circleCollider.enabled;
+    }
+    private void SwitchCollider(bool enabled)
+    {
+        if (enabled)
+        {
+            boxCollider.enabled = false;
+            circleCollider.enabled = true;
+        } else
+        {
+            boxCollider.enabled = true;
+            circleCollider.enabled = false;
+        }
     }
 
 }
